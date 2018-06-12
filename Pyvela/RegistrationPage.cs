fc6930fe908d464184a3a0f4f51744a801752e98
sultan841 <38570748+sultan841@users.noreply.github.com>
2018-06-12 12:57:27 +0600

    aa

---
+++ Pyvela/RegistrationPage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Pyvela
+{
+    class RegistrationPage
+    {
+
+        [Activity(Label = "Login", MainLauncher = true)]
+        public class MainActivity : Activity
+        {
+
+            protected override void OnCreate(Bundle bundle)
+            {
+                base.OnCreate(bundle);
+
+                SetContentView(Resource.Layout.RegistrationPage);
+
+
+            }
+        }
+    }
